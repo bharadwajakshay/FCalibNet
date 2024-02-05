616309e0cc3f8755531a1a35e5de24f991dc12ba
Akshay Bharadwaj <akshay.bharadwaj@ucalgary.ca>
2024-02-05 11:06:08 -0700

    Added checkpoint filename to load checkpoint

--- src/config.py
+++ src/config.py
@@ -4,15 +4,18 @@ checkpointDir = os.path.join(os.getcwd(), 'checkpoints')
 logDir = os.path.join(os.getcwd(),'logs')
 DDPT = False # Distributed Data Parallel Training
 DPT = False # Data Parallel Training
+modelParallel = True # Need this for Unet3D
 training = dict(
-    batchSize = 20,
-    epoch = 25,
+    batchSize = 35,
+    epoch = 50,
     learningRate = 0.005,
     beta0 = 0.9,
     beta1 = 0.999,
     eps = 1e-08,
     decayRate = 1e-4,
+    loadFromCheckpoint = True
 )
+chkPointFileName = '2024_2_3_21:18_ED_2.0538178968429563.pth'
 '''
     # Possible value are 
     "EUC" = Euclidean distance between points
@@ -21,8 +24,6 @@ training = dict(
     "EMD" = Earthmovers distance between points
     "CHORDAL" = Chordal distance between the predicted and the target transforms
     "EUCTR" = Euclidean distance between the translation of the predicated transform and the ground truth
-
- 
 '''
 
 loss = ["CHAMP","CHORDAL","EUCTR"]
